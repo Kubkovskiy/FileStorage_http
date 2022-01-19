e31f1f6e3514d78f5d460af715a0ba4460d95b9d
Kubkovskiy <Kubkovskiy@gmail.com>
2022-01-19 13:57:44 +0300

    add seup and terdown to test_post

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,16 +1,23 @@
+import datetime
 import json
+import os
+import zipfile
 import requests
 from requests import Response
 from lib.assertions import Assertions
 
 
 class BaseCase:
-    def get_header(self, response: Response, header_name):
+    FILES_FOR_UPLOAD = 'tests/files_for_upload/'
+
+
+    @staticmethod
+    def get_header(response: Response, header_name):
         assert header_name in response.headers, f"Cannot find header with the name {header_name} \
                                                 in response"
         return response.headers[header_name]
-
-    def get_json_value(self, response: Response, name):
+    @staticmethod
+    def get_json_value(response: Response, name):
         try:
             response_as_dict = response.json()
         except json.decoder.JSONDecodeError:
@@ -19,8 +26,7 @@ class BaseCase:
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
         return response_as_dict[name]
 
-    def download_file_from_cloud(self, url: str) -> tuple:
-
+    def download_file_from_cloud(self, url: str):
         """Download file from yandex disk. return (content-type:str, headers:dict, file:bytes) """
         base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key='
         # Получаем загрузочную ссылку
@@ -28,14 +34,39 @@ class BaseCase:
         response = requests.get(final_url)
         Assertions.assert_expected_status_code(response, 200)
         download_url = self.get_json_value(response, 'href')
-
         # Загружаем файл и возвращаем его
         download_response = requests.get(download_url)
         Assertions.assert_expected_status_code(download_response, 200)
-        content_type = download_response.headers.get('Content-Type')
-        headers = {
-            'Content-Disposition': download_response.headers.get('Content-Disposition'),
-            'Content-Length': download_response.headers.get('Content-Length'),
-        }
+        return download_response
 
-        return (content_type, headers, download_response.content)
+    def download_and_extract_zip_archive_from_cloud(self, url: str, filename: str = None) -> list:
+        """
+        url - address to folder 'files_for_upload' on cloud. filename = test_+time_now.zip(default),
+        path to upload_folder = 'tests/files_for_upload/
+        First will download archive and save with 'filename' at 'path'\
+        then extract 'files_for_upload' folder to 'tests/' dir
+        after delete archive
+        """
+        content = self.download_file_from_cloud(url).content
+        modification_time = str(datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+        # Check filename and path if None use defaut
+        base_name = filename if filename else f'test_{modification_time}.zip'
+        path_to_upload_folder = BaseCase.FILES_FOR_UPLOAD
+        full_name = path_to_upload_folder + base_name
+        # check is there 'tests/files_for_upload/ folder if not should create
+        if not os.path.isdir(path_to_upload_folder):
+            os.mkdir(path_to_upload_folder)
+        assert os.path.isdir(path_to_upload_folder), f'There are not folder {path_to_upload_folder}'
+        # check is file zip archive and extract it
+        with open(full_name, 'wb') as f:
+            f.write(content)
+        assert zipfile.is_zipfile(full_name), f"Content from cloud isn't zip archive"
+        with zipfile.ZipFile(full_name, 'r') as z:
+            z.extractall('tests/')
+        # remove archive
+        os.remove(full_name)
+        # return list with names of file
+        names_list = os.listdir(path_to_upload_folder)
+        # check is folder not empty
+        assert len(names_list) > 0, f"Files_for_upload is empty"
+        return names_list
--- tests/test_post_method.py
+++ tests/test_post_method.py
@@ -1,13 +1,46 @@
+import os
+
+import pytest
+
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from lib.my_requests import MyRequests
 import requests
+import pytest
+
+
+# @pytest.fixture(scope='class')
+# def prepair_files_for_upload():
+#     url_to_archive = "https://disk.yandex.ru/d/aJoFOPqLRHGGXw"
+#     files = BaseCase.download_and_extract_zip_archive_from_cloud(url_to_archive)
+#     print(files)
 
 
 class TestPostMethod(BaseCase):
+    def setup_method(self, test_method):
+        url_to_archive = "https://disk.yandex.ru/d/aJoFOPqLRHGGXw"
+        self.files = self.download_and_extract_zip_archive_from_cloud(url_to_archive)
+
+
+    def teardown_method(self, test_method):
+        import shutil
+        shutil.rmtree(BaseCase.FILES_FOR_UPLOAD)
+
+
+    def test_is_file_in_dir(self):
+        print(os.listdir('tests/files_for_upload/'))
+        assert self.files == os.listdir('tests/files_for_upload/'), f'что то не так'
+
+
     def test_post_with_empty_data(self):
         file_url = "https://disk.yandex.ru/i/2xwFm2zp34nzZA"
-        content_type, headers, file_from_cloud = self.download_file_from_cloud(file_url)
+        response_from_cloud = self.download_file_from_cloud(file_url)
+        content_type = response_from_cloud.headers.get('Content-Type')
+        file_from_cloud = response_from_cloud.content
+        headers = {
+            'Content-Disposition': response_from_cloud.headers.get('Content-Disposition'),
+            'Content-Length': response_from_cloud.headers.get('Content-Length'),
+        }
         payload = {"file": file_from_cloud}
         data = {"name": None, "tag": None, "id": None, "content-type": content_type}
         response = MyRequests.post("upload", data, headers, payload)
@@ -17,7 +50,13 @@ class TestPostMethod(BaseCase):
 
     def test_post_with_id(self):
         file_url = "https://disk.yandex.ru/i/2xwFm2zp34nzZA"
-        content_type, headers, file_from_cloud = self.download_file_from_cloud(file_url)
+        response_from_cloud = self.download_file_from_cloud(file_url)
+        content_type = response_from_cloud.headers.get('Content-Type')
+        file_from_cloud = response_from_cloud.content
+        headers = {
+            'Content-Disposition': response_from_cloud.headers.get('Content-Disposition'),
+            'Content-Length': response_from_cloud.headers.get('Content-Length'),
+                }
         payload = {"file": file_from_cloud}
         file_id = 1
         data = {"name": None, "tag": None, "id": file_id, "content-type": content_type}
@@ -30,7 +69,13 @@ class TestPostMethod(BaseCase):
 
     def test_post_with_name(self):
         file_url = "https://disk.yandex.ru/i/2xwFm2zp34nzZA"
-        content_type, headers, file_from_cloud = self.download_file_from_cloud(file_url)
+        response_from_cloud = self.download_file_from_cloud(file_url)
+        content_type = response_from_cloud.headers.get('Content-Type')
+        file_from_cloud = response_from_cloud.content
+        headers = {
+            'Content-Disposition': response_from_cloud.headers.get('Content-Disposition'),
+            'Content-Length': response_from_cloud.headers.get('Content-Length'),
+        }
         payload = {"file": file_from_cloud}
         name = 'test_name'
         data = {"name": name, "tag": None, "id": None, "content-type": content_type}
@@ -43,7 +88,13 @@ class TestPostMethod(BaseCase):
 
     def test_post_with_name_and_id(self):
         file_url = "https://disk.yandex.ru/i/2xwFm2zp34nzZA"
-        content_type, headers, file_from_cloud = self.download_file_from_cloud(file_url)
+        response_from_cloud = self.download_file_from_cloud(file_url)
+        content_type = response_from_cloud.headers.get('Content-Type')
+        file_from_cloud = response_from_cloud.content
+        headers = {
+            'Content-Disposition': response_from_cloud.headers.get('Content-Disposition'),
+            'Content-Length': response_from_cloud.headers.get('Content-Length'),
+        }
         payload = {"file": file_from_cloud}
         name = 'test_name'
         file_id = 15
