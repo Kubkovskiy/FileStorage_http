0051e2a48eac948cfdfa1168f3131810bae2ffdc
Aleksey.Kubkovskiy <aleksey.kubkovskiy@auriga.com>
2022-01-27 19:36:36 +0300

    update test for get method

--- tests/test_get_method.py
+++ tests/test_get_method.py
@@ -11,9 +11,7 @@ class TestGetMethod(BaseCase):
         # delete files from FileStorage
         response = cls.delete_all_files_from_server()
 
-
     def teardown_class(cls):
-
         # delete files from FileStorage
         cls.delete_all_files_from_server()
         # delete tests/files_for_upload/
@@ -35,3 +33,63 @@ class TestGetMethod(BaseCase):
         assert amount_of_files == amount_of_files_from_response, \
             f"Number of received files ({amount_of_files_from_response}) not equal number of  \
             sent files ({amount_of_files})"
+
+    def test_get_with_id(self):
+        # upload file with file_id
+        files_for_test = self.get_files()
+        list_id = [10, 20, 30, 40, 50]
+        for i in range(len(files_for_test)):
+            file = files_for_test[i]
+            file_dict = self.open_file_from_upload_folder(file)
+            file_id = list_id[i]
+            data, headers, payload = self.set_data_to_post_method(file_dict, file_id=file_id)
+            response_post = MyRequests.post("upload", data, headers, payload)
+            Assertions.base_assertions_for_post_method(response_post)
+        # test get
+        expected_id = []
+        data = {"id": expected_id}
+        count = 0
+        for file_id in list_id:
+            expected_id.append(file_id)
+            count += 1
+            response = MyRequests.get('get', params=data)
+            response_list = response.json()
+            Assertions.base_assertions_for_get_method(response)
+            num_of_files = len(response_list)
+            assert num_of_files == count, \
+                f"Number of files received ({num_of_files}) is not equal to the expected number ({count})"
+            received_id = []
+            for meta in response_list:
+                received_id.append(meta['id'])
+            assert expected_id == received_id, \
+                f"Unexpected id! expected - {expected_id}, actual - {received_id}"
+
+    def test_get_with_name(self):
+        # upload file with file_id
+        files_for_test = self.get_files()
+        list_name = ['test1', 'test2', 'test3', 'test4', 'test5']
+        for i in range(len(files_for_test)):
+            file = files_for_test[i]
+            file_dict = self.open_file_from_upload_folder(file)
+            name = list_name[i]
+            data, headers, payload = self.set_data_to_post_method(file_dict, name=name)
+            response_post = MyRequests.post("upload", data, headers, payload)
+            Assertions.base_assertions_for_post_method(response_post)
+        # test get
+        expected_name = []
+        data = {"name": expected_name}
+        count = 0
+        for name in list_name:
+            expected_name.append(name)
+            count += 1
+            response = MyRequests.get('get', params=data)
+            response_list = response.json()
+            Assertions.base_assertions_for_get_method(response)
+            num_of_files = len(response_list)
+            assert num_of_files == count, \
+                f"Number of files received ({num_of_files}) is not equal to the expected number ({count})"
+            received_name = []
+            for meta in response_list:
+                received_name.append(meta['name'])
+            assert expected_name == received_name, \
+                f"Unexpected name! expected - {expected_name}, actual - {received_name}"
