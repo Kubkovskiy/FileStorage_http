53414c5bec75394ec64ba64f0b3c0920ef5a9e81
Aleksey.Kubkovskiy <aleksey.kubkovskiy@auriga.com>
2022-02-02 10:27:12 +0300

    add fixture preparing_files_for_upload_and_delete_after_test to conftest, remove setup and teardown from tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,18 @@
-from lib.assertions import Assertions
 from lib.base_case import BaseCase
-from lib.my_requests import MyRequests
 import pytest
-import shutil
-import os
 
 
 @pytest.fixture(scope='session', autouse=True)
-def preparing_files_for_upload_and_delete_after_test():
-    BaseCase.delete_all_files_from_server()
+def preparing_files_for_upload_and_delete_after_session():
     files = BaseCase.get_files()
     yield files
-    # delete files from FileStorage
-    BaseCase.delete_all_files_from_server()
     # delete tests/files_for_upload/
     BaseCase.delete_upload_files()
+
+
+@pytest.fixture(scope='class', autouse=True)
+def delete_files_from_server():
+    BaseCase.delete_all_files_from_server()
+    yield
+    # delete files from FileStorage
+    BaseCase.delete_all_files_from_server()
